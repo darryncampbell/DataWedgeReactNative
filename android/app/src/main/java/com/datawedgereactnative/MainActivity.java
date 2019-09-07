9b78d1a53fec36bfa89680812d912920f7281d85
Luke Paireepinart <luke.paireepinart@mccoys.com>
2019-09-07 13:41:27 -0500

    Merge additional changes for React Native update

--- android/app/src/main/java/com/datawedgereactnative/MainActivity.java
+++ android/app/src/main/java/com/datawedgereactnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tempproject;
+package com.datawedgereactnative.demo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "tempProject";
+        return "DataWedgeReactNative";
     }
 }
